7ef92050c85cdbbab6156be1a8c1b2e40c5980db
kulgaurav <kulgaurav23@gmail.com>
2020-04-18 11:45:13 +0100

    Redux addition | Component linking

--- react-client/src/components/Activity/NewActivityButton.js
+++ react-client/src/components/Activity/NewActivityButton.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NewActivityButton = () => {
-  <React.Fragment>
-    <Link to="/addActivity" className="btn btn-lg btn-info">
-      Create a Project
-    </Link>
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <Link to="/addActivity" className="btn btn-lg btn-info">
+        Create a Project
+      </Link>
+    </React.Fragment>
+  );
 };
 
 export default NewActivityButton;
